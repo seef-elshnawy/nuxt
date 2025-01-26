baf0465b52ab49a1ad96b4f1d2bfa3e3bec425ff
Seef Elshnawy <elshnawyseef@gmail.com>
2025-01-26 13:10:02 +0200

    init

---
+++ nuxt-ecommarce/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet quis
+      dolorum esse perferendis. Autem, repellat? Dolor quibusdam et placeat
+      adipisci provident at corrupti dignissimos excepturi rem? Laudantium
+      debitis doloribus qui.
+    </p>
+  </div>
+</template>
---
+++ nuxt-ecommarce/nuxt.config.ts
@@ -0,0 +1,14 @@
+import * as path from "path";
+import tailwindcss from "@tailwindcss/vite";
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  alias: {
+    "@": path.resolve(__dirname, "src"),
+  },
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
